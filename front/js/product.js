3fce721eadd667e7114037cd99c54e9cd1bbbd05
Vincent M <vincentmartins52@yahoo.fr>
2022-02-17 15:03:37 +0100

    indenter product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+// Récupération de l'ID au sein de l'URL
 var str =
   "file:///C:/Users/vince/Documents/VincentMartins_5_20122021/front/html/product.html?id=415b7cacb65d43b2b5c1ff70f3393ad1";
 var url = new URL(window.location.href);
@@ -9,39 +10,35 @@ const title = document.getElementById("title");
 const price = document.getElementById("price");
 const description = document.getElementById("description");
 
+// Affichage du produit en fonction de son ID
 fetch("http://localhost:3000/api/products/" + products)
   .then((response) => response.json())
   .then((product) => {
+    //Insertion des éléments titre, prix et description
     title.innerHTML = product.name;
     price.innerHTML = product.price;
     description.innerHTML = product.description;
 
+    // Insertion de l'image
     let image = product.imageUrl;
     console.log(image);
-
     var vincent = `<img src="${image}" alt="${product.altTxt}">`;
     const item__imgs = document.getElementsByClassName("item__img");
     const item_img = item__imgs[0];
     item_img.innerHTML = vincent;
 
+    //Insertion des modifications de la couleur
     product.colors.forEach((color) => {
       const couleurs = `<option value="${color}">${color}</option>`;
       document.getElementById("colors").innerHTML += couleurs;
     });
 
+    //Gestion de la quantité et des couleurs choisies
     const panier = (addToCart.onclick = () => {
-      /*console.log(products);
-      localStorage.setItem("id", products);*/
-
       const quantityLs = parseInt(document.getElementById("quantity").value);
-      /*console.log(quantityLs);
-      localStorage.setItem("quantity", quantityLs);*/
-
       const colorsLs = document.getElementById("colors").value;
-      /*console.log(colorsLs);
 
-      localStorage.setItem("couleur", colorsLs);*/
-
+      // récupération des options du produit
       var canap = {
         id: products,
         quantity: quantityLs,
@@ -53,19 +50,24 @@ fetch("http://localhost:3000/api/products/" + products)
       };
       console.log(canap);
 
+      // Initialisation du localStorage
       let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
       console.log(produitLocalStorage);
 
+      // Alerte d'ajout au panier
       const confirmation = () => {
         if (window.alert("Produit ajouté au panier")) {
           window.location.href = "cart.html";
         }
       };
+
+      // importation dans le LocalStorage
       if (produitLocalStorage) {
         const resultFind = produitLocalStorage.find(
           (el) => el.id === products && el.colors === colorsLs
         );
 
+        //Modification de la quantité dans le LocalStorage
         if (resultFind) {
           let newQuantite =
             parseInt(canap.quantity) + parseInt(resultFind.quantity);
@@ -73,12 +75,14 @@ fetch("http://localhost:3000/api/products/" + products)
           localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
           console.table(produitLocalStorage);
           confirmation();
+          //Ajout du produit dans le LocalStorage
         } else {
           produitLocalStorage.push(canap);
           localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
           console.table(produitLocalStorage);
           confirmation();
         }
+        //Création array dans le LocalStorage
       } else {
         produitLocalStorage = [];
         produitLocalStorage.push(canap);

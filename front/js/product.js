d947f5fc128d0af2107babd1d8c39a89a16e670a
Vincent M <vincentmartins52@yahoo.fr>
2022-02-18 18:13:44 +0100

    modif product

--- front/js/product.js
+++ front/js/product.js
@@ -14,95 +14,100 @@ const description = document.getElementById("description");
 fetch("http://localhost:3000/api/products/" + products)
   .then((response) => response.json())
   .then((product) => {
-    //Insertion des éléments titre, prix et description
-    title.innerHTML = product.name;
-    price.innerHTML = product.price;
-    description.innerHTML = product.description;
+    function addElements(product) {
+      //Insertion des éléments titre, prix et description
+      title.innerHTML = product.name;
+      price.innerHTML = product.price;
+      description.innerHTML = product.description;
 
-    // Insertion de l'image
-    let image = product.imageUrl;
-    console.log(image);
-    var vincent = `<img src="${image}" alt="${product.altTxt}">`;
-    const item__imgs = document.getElementsByClassName("item__img");
-    const item_img = item__imgs[0];
-    item_img.innerHTML = vincent;
+      // Insertion de l'image
+      let image = product.imageUrl;
+      console.log(image);
+      var vincent = `<img src="${image}" alt="${product.altTxt}">`;
+      const item__imgs = document.getElementsByClassName("item__img");
+      const item_img = item__imgs[0];
+      item_img.innerHTML = vincent;
 
-    //Insertion des modifications de la couleur
-    product.colors.forEach((color) => {
-      const couleurs = `<option value="${color}">${color}</option>`;
-      document.getElementById("colors").innerHTML += couleurs;
-    });
+      //Insertion des modifications de la couleur
+      product.colors.forEach((color) => {
+        const couleurs = `<option value="${color}">${color}</option>`;
+        document.getElementById("colors").innerHTML += couleurs;
+      });
+    }
+    addElements(product);
+    addLS(product);
+  });
+function addLS(product) {
+  //Gestion de la quantité et des couleurs choisies
+  const panier = (addToCart.onclick = () => {
+    const quantityLs = parseInt(document.getElementById("quantity").value);
+    const colorsLs = document.getElementById("colors").value;
 
-    //Gestion de la quantité et des couleurs choisies
-    const panier = (addToCart.onclick = () => {
-      const quantityLs = parseInt(document.getElementById("quantity").value);
-      const colorsLs = document.getElementById("colors").value;
+    // Si aucune couleur n'est sélectionnée
+    if (colorsLs == "") {
+      alert("Veuillez sélectionner une couleur");
+      return;
+    }
 
-      // Si aucune couleur n'est sélectionnée
-      if (colorsLs == "") {
-        alert("Veuillez sélectionner une couleur");
-        return;
+    // Si la quantité est égale à 0
+    if (quantityLs == 0) {
+      alert("Veuillez sélectionner une quantité");
+      return;
+    } else if (quantityLs > 100) {
+      alert("La quantité maximale autorisée est de 100 unités");
+      return;
+    }
+    // récupération des options du produit
+    var canap = {
+      id: products,
+      quantity: quantityLs,
+      colors: colorsLs,
+      name: product.name,
+      image: product.imageUrl,
+      alt: product.altTxt,
+      price: product.price,
+    };
+    console.log(canap);
+
+    // Initialisation du localStorage
+    let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+    console.log(produitLocalStorage);
+
+    // Alerte d'ajout au panier
+    const confirmation = () => {
+      if (window.alert("Produit ajouté au panier")) {
+        window.location.href = "cart.html";
       }
+    };
 
-      // Si la quantité est égale à 0
-      if (quantityLs == 0) {
-        alert("Veuillez sélectionner une quantité");
-        return;
-      } else if (quantityLs > 100) {
-        alert("La quantité maximale autorisée est de 100 unités");
-        return;
-      }
-      // récupération des options du produit
-      var canap = {
-        id: products,
-        quantity: quantityLs,
-        colors: colorsLs,
-        name: product.name,
-        image: product.imageUrl,
-        alt: product.altTxt,
-        price: product.price,
-      };
-      console.log(canap);
+    // importation dans le LocalStorage
+    if (produitLocalStorage) {
+      const resultFind = produitLocalStorage.find(
+        (el) => el.id === products && el.colors === colorsLs
+      );
 
-      // Initialisation du localStorage
-      let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
-      console.log(produitLocalStorage);
-
-      // Alerte d'ajout au panier
-      const confirmation = () => {
-        if (window.alert("Produit ajouté au panier")) {
-          window.location.href = "cart.html";
-        }
-      };
-
-      // importation dans le LocalStorage
-      if (produitLocalStorage) {
-        const resultFind = produitLocalStorage.find(
-          (el) => el.id === products && el.colors === colorsLs
-        );
-
-        //Modification de la quantité dans le LocalStorage
-        if (resultFind) {
-          let newQuantite =
-            parseInt(canap.quantity) + parseInt(resultFind.quantity);
-          resultFind.quantity = newQuantite;
-          localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
-          console.table(produitLocalStorage);
-          confirmation();
-          //Ajout du produit dans le LocalStorage
-        } else {
-          produitLocalStorage.push(canap);
-          localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
-          console.table(produitLocalStorage);
-          confirmation();
-        }
-        //Création array dans le LocalStorage
+      //Modification de la quantité dans le LocalStorage
+      if (resultFind) {
+        let newQuantite =
+          parseInt(canap.quantity) + parseInt(resultFind.quantity);
+        resultFind.quantity = newQuantite;
+        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+        console.table(produitLocalStorage);
+        confirmation();
+        //Ajout du produit dans le LocalStorage
       } else {
-        produitLocalStorage = [];
         produitLocalStorage.push(canap);
         localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
         console.table(produitLocalStorage);
         confirmation();
       }
-    });
+      //Création array dans le LocalStorage
+    } else {
+      produitLocalStorage = [];
+      produitLocalStorage.push(canap);
+      localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+      console.table(produitLocalStorage);
+      confirmation();
+    }
   });
+}

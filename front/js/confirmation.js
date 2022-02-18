f5a13621619b010b1df21aaccf5a8bc9ef123893
Vincent M <vincentmartins52@yahoo.fr>
2022-02-18 17:32:18 +0100

    Indeter confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,35 +1,11 @@
 function commande() {
+  //Ajout de l'élément dans le HTML
   const bilan = document.getElementById("orderId");
-  console.log("bilan");
   console.log(bilan);
+  //Récupération de l'id de la commande
   var url = new URL(window.location.href);
   var orderId = url.searchParams.get("orderId");
   console.log(orderId);
   bilan.innerHTML = orderId;
 }
 commande();
-
-// const apiParam = {
-//   method: "POST",
-//   headers: {
-//     Accept: "application/json",
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify(order),
-// };
-
-// fetch("http://localhost:3000/api/products/order", apiParam)
-//   .then((data) => data.json())
-
-//   .then((order) => {
-//     let htmlOrderId = document.getElementById("orderId");
-//     let newOrderId = document.createTextNode(`${order.orderId}`);
-
-//     if (newOrderId != undefined) {
-//       htmlOrderId.replaceChild(newOrderId, htmlOrderId.childNodes[0]);
-//     } else {
-//       htmlOrderId.appendChild(newOrderId);
-//     }
-//   });
-
-// localStorage.clear();
